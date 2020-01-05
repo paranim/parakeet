9d8b6c58dc2c9439280ded9db175df3920759a19
oakes <zach@sekao.net>
2020-01-05 15:16:56 -0500

    Rename project and move rectangle example out

---
+++ parakeet.nimble
@@ -0,0 +1,13 @@
+# Package
+
+version       = "0.1.0"
+srcDir        = "src"
+bin           = @["parakeet"]
+
+
+
+# Dependencies
+
+requires "nim >= 1.0.4"
+requires "paranim >= 0.1.0"
+requires "stb_image >= 2.5"
--- src/parakeet.nim
+++ src/parakeet.nim
@@ -1,34 +1,45 @@
 import nimgl/[glfw, opengl]
+import stb_image/read as stbi
 import glm
-import parabola/utils, parabola/math
+import paranim/gl, paranim/gl/utils, paranim/math
+
+converter toSeqUint8(s: string): seq[uint8] = cast[seq[uint8]](s)
 
 proc keyProc(window: GLFWWindow, key: int32, scancode: int32,
              action: int32, mods: int32): void {.cdecl.} =
   if key == GLFWKey.ESCAPE and action == GLFWPress:
     window.setWindowShouldClose(true)
 
-const twoDVertexShader =
+const imageVertexShader =
   """
   #version 410
   uniform mat3 u_matrix;
+  uniform mat3 u_texture_matrix;
   in vec2 a_position;
+  out vec2 v_tex_coord;
   void main()
   {
     gl_Position = (vec4(((u_matrix * (vec3(a_position, 1))).xy), 0, 1));
+    v_tex_coord = ((u_texture_matrix * (vec3(a_position, 1))).xy);
   }
   """
 
-const twoDFragmentShader =
+const imageFragmentShader =
   """
   #version 410
   precision mediump float;
-  uniform vec4 u_color;
+  uniform sampler2D u_image;
+  in vec2 v_tex_coord;
   out vec4 o_color;
   void main()
   {
-    o_color = u_color;
+    o_color = (texture(u_image, v_tex_coord));
   }
   """
+
+const playerWalk1 = staticRead("assets/player_walk1.png")
+const playerWalk2 = staticRead("assets/player_walk2.png")
+const playerWalk3 = staticRead("assets/player_walk3.png")
 
 const rect =
   @[0f, 0f,
@@ -63,7 +74,7 @@ proc main() =
 
   let game = Game(texCount: 0)
 
-  let program = createProgram(twoDVertexShader, twoDFragmentShader)
+  let program = createProgram(imageVertexShader, imageFragmentShader)
   glUseProgram(program)
   var vao: GLuint
   glGenVertexArrays(1, vao.addr)
@@ -73,18 +84,42 @@ proc main() =
   glGenBuffers(1, positionBuf.addr)
   let drawCount = setArrayBuffer(program, positionBuf, "a_position", Attribute(data: rect, size: 2))
 
-  let matrixUni = glGetUniformLocation(program, "u_matrix")
+  var
+    width, height, channels: int
+    data: seq[uint8]
+  data = stbi.loadFromMemory(playerWalk1, width, height, channels, stbi.Default)
+
+  var imageUni = glGetUniformLocation(program, "u_image")
+  let opts = Opts(
+    mipLevel: 0,
+    internalFmt: GL_RGBA,
+    width: GLsizei(width),
+    height: GLsizei(height),
+    border: 0,
+    srcFmt: GL_RGBA,
+    srcType: GL_UNSIGNED_BYTE
+  )
+  let params = @[
+    (GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE),
+    (GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE),
+    (GL_TEXTURE_MIN_FILTER, GL_NEAREST),
+    (GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+  ]
+  let unit = createTexture(game, imageUni, data, opts, params)
+  glUniform1i(imageUni, unit)
+
+  var textureMatrixUni = glGetUniformLocation(program, "u_texture_matrix")
+  var textureMatrix = identityMatrix()
+  glUniformMatrix3fv(textureMatrixUni, 1, false, textureMatrix.caddr)
+
+  var matrixUni = glGetUniformLocation(program, "u_matrix")
   var matrix = (
-     scalingMatrix(50f, 50f) *
-     translationMatrix(50f, 50f) *
-     projectionMatrix(800f, 600f) *
-     identityMatrix()
+    scalingMatrix(cfloat(width), cfloat(height)) *
+    translationMatrix(0f, 0f) *
+    projectionMatrix(800f, 600f) *
+    identityMatrix()
   ).transpose()
   glUniformMatrix3fv(matrixUni, 1, false, matrix.caddr)
-
-  let colorUni = glGetUniformLocation(program, "u_color")
-  var color = vec4(1f, 0f, 0f, 1)
-  glUniform4fv(colorUni, 1, color.caddr)
 
   while not w.windowShouldClose:
     glClearColor(173/255, 216/255, 230/255, 1f)
