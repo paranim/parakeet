df6288ecb9e3de692980f55102a9f87887cb0f3c
oakes <zach@sekao.net>
2020-01-29 16:22:25 -0500

    Move glfw stuff into main module

--- src/core.nim
+++ src/core.nim
@@ -1,25 +1,13 @@
-import nimgl/[glfw, opengl]
+import nimgl/opengl
 import stb_image/read as stbi
 import glm
 import paranim/gl, paranim/gl/utils, paranim/gl/entities2d, paranim/primitives2d
 import tables
 
-when not defined(release):
-  import hotcodereloading
-
 type
-  Game = object of RootGame
+  Game* = object of RootGame
 
 converter toSeqUint8(s: string): seq[uint8] = cast[seq[uint8]](s)
-
-proc keyProc(window: GLFWWindow, key: int32, scancode: int32,
-             action: int32, mods: int32): void {.cdecl.} =
-  if key == GLFWKey.ESCAPE and action == GLFWPress:
-    window.setWindowShouldClose(true)
-
-  when not defined(release):
-    if key == GLFWKey.ENTER and action == GLFWPress:
-      performCodeReload()
 
 const playerWalk1 = staticRead("assets/player_walk1.png")
 const playerWalk2 = staticRead("assets/player_walk2.png")
@@ -27,30 +15,13 @@ const playerWalk3 = staticRead("assets/player_walk3.png")
 
 var image: ImageEntity
 
-proc init*(): GLFWWindow =
-  assert glfwInit()
-
-  glfwWindowHint(GLFWContextVersionMajor, 4)
-  glfwWindowHint(GLFWContextVersionMinor, 1)
-  glfwWindowHint(GLFWOpenglForwardCompat, GLFW_TRUE) # Used for Mac
-  glfwWindowHint(GLFWOpenglProfile, GLFW_OPENGL_CORE_PROFILE)
-  glfwWindowHint(GLFWResizable, GLFW_TRUE)
-
-  let w: GLFWWindow = glfwCreateWindow(800, 600, "NimGL")
-  if w == nil:
-    quit(-1)
-
-  discard w.setKeyCallback(keyProc)
-  w.makeContextCurrent()
-
+proc init*(game: var Game) =
   assert glInit()
 
   glEnable(GL_BLEND)
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
   glDisable(GL_CULL_FACE)
   glDisable(GL_DEPTH_TEST)
-
-  var game = Game(texCount: 0)
 
   var
     width, height, channels: int
@@ -77,22 +48,10 @@ proc init*(): GLFWWindow =
   ).transpose()
   glUniformMatrix3fv(matrixUni, 1, false, matrix.caddr)
 
-  w
-
-proc run*(w: GLFWWindow): bool =
-  not w.windowShouldClose
-
-proc update*(w: GLFWWindow) =
+proc tick*(game: Game) =
   glClearColor(173/255, 216/255, 230/255, 1f)
   glClear(GL_COLOR_BUFFER_BIT)
   glViewport(0, 0, 800, 600)
 
   glDrawArrays(GL_TRIANGLES, 0, image.drawCount)
-
-  w.swapBuffers()
-  glfwPollEvents()
-
-proc destroy*(w: GLFWWindow) =
-  w.destroyWindow()
-  glfwTerminate()
 
--- src/parakeet.nim
+++ src/parakeet.nim
@@ -1,9 +1,41 @@
+import nimgl/glfw
 import core
 
-proc main() =
-  let w = init()
-  while w.run():
-    w.update()
-  w.destroy()
+when not defined(release):
+  import hotcodereloading
 
-main()
+proc keyProc(window: GLFWWindow, key: int32, scancode: int32,
+             action: int32, mods: int32): void {.cdecl.} =
+  if key == GLFWKey.ESCAPE and action == GLFWPress:
+    window.setWindowShouldClose(true)
+
+  when not defined(release):
+    if key == GLFWKey.ENTER and action == GLFWPress:
+      performCodeReload()
+
+when isMainModule:
+  assert glfwInit()
+
+  glfwWindowHint(GLFWContextVersionMajor, 4)
+  glfwWindowHint(GLFWContextVersionMinor, 1)
+  glfwWindowHint(GLFWOpenglForwardCompat, GLFW_TRUE) # Used for Mac
+  glfwWindowHint(GLFWOpenglProfile, GLFW_OPENGL_CORE_PROFILE)
+  glfwWindowHint(GLFWResizable, GLFW_TRUE)
+
+  let w: GLFWWindow = glfwCreateWindow(800, 600, "NimGL")
+  if w == nil:
+    quit(-1)
+
+  discard w.setKeyCallback(keyProc)
+  w.makeContextCurrent()
+
+  var game = Game()
+  game.init()
+
+  while not w.windowShouldClose:
+    game.tick()
+    w.swapBuffers()
+    glfwPollEvents()
+
+  w.destroyWindow()
+  glfwTerminate()
