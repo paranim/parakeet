fd9e20ac0195ab7ca61ae36e675e4ef2c9a454c5
oakes <zach@sekao.net>
2020-02-05 07:13:54 -0500

    Use framebuffer size instead of window size

--- src/parakeet.nim
+++ src/parakeet.nim
@@ -40,7 +40,7 @@ when isMainModule:
   discard w.setKeyCallback(keyProc)
   discard w.setMouseButtonCallback(mouseButtonProc)
   discard w.setCursorPosCallback(mousePositionProc)
-  discard w.setWindowSizeCallback(resizeProc)
+  discard w.setFramebufferSizeCallback(resizeProc)
   var width, height: int32
   w.getFramebufferSize(width.addr, height.addr)
   w.resizeProc(width, height)
