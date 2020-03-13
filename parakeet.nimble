1a78846f7c194a080d7c6e93d1d70bf4172ebe75
oakes <zach@sekao.net>
2020-03-12 22:20:32 -0400

    Add paravim

--- parakeet.nimble
+++ parakeet.nimble
@@ -12,3 +12,6 @@ requires "nim >= 1.0.4"
 requires "paranim >= 0.3.0"
 requires "pararules >= 0.2.0"
 requires "stb_image >= 2.5"
+
+when not defined(release):
+  requires "paravim >= 0.2.0"
--- src/parakeet.nim
+++ src/parakeet.nim
@@ -1,23 +1,48 @@
 import nimgl/glfw
 import core
 
+when not defined(release):
+  from paravim import nil
+  var focusOnGame = true
+
 proc keyCallback(window: GLFWWindow, key: int32, scancode: int32, action: int32, mods: int32) {.cdecl.} =
-  if action == GLFW_PRESS:
-    if key == GLFWKey.Escape:
-      window.setWindowShouldClose(true)
+  when not defined(release):
+    if action == GLFW_PRESS and key == GLFWKey.Escape and paravim.isNormalMode():
+      focusOnGame = not focusOnGame
+      return
     else:
-      onKeyPress(key)
+      if not focusOnGame:
+        paravim.keyCallback(window, key, scancode, action, mods)
+        return
+  if action == GLFW_PRESS:
+    onKeyPress(key)
   elif action == GLFW_RELEASE:
     onKeyRelease(key)
 
+proc charCallback(window: GLFWWindow, codepoint: uint32) {.cdecl.} =
+  when not defined(release):
+    if not focusOnGame:
+      paravim.charCallback(window, codepoint)
+      return
+
 proc mouseButtonCallback(window: GLFWWindow, button: int32, action: int32, mods: int32) {.cdecl.} =
+  when not defined(release):
+    if not focusOnGame:
+      paravim.mouseButtonCallback(window, button, action, mods)
+      return
   if action == GLFWPress:
     onMouseClick(button)
 
 proc cursorPosCallback(window: GLFWWindow, xpos: float64, ypos: float64) {.cdecl.} =
+  when not defined(release):
+    if not focusOnGame:
+      paravim.cursorPosCallback(window, xpos, ypos)
+      return
   onMouseMove(xpos, ypos)
 
 proc frameSizeCallback(window: GLFWWindow, width: int32, height: int32) {.cdecl.} =
+  when not defined(release):
+    paravim.frameSizeCallback(window, width, height)
   onWindowResize(width, height)
 
 when isMainModule:
@@ -29,7 +54,7 @@ when isMainModule:
   glfwWindowHint(GLFWOpenglProfile, GLFW_OPENGL_CORE_PROFILE)
   glfwWindowHint(GLFWResizable, GLFW_TRUE)
 
-  let w: GLFWWindow = glfwCreateWindow(800, 600, "Parakeet")
+  let w: GLFWWindow = glfwCreateWindow(1024, 768, "Parakeet")
   if w == nil:
     quit(-1)
 
@@ -37,15 +62,20 @@ when isMainModule:
   glfwSwapInterval(1)
 
   discard w.setKeyCallback(keyCallback)
+  discard w.setCharCallback(charCallback)
   discard w.setMouseButtonCallback(mouseButtonCallback)
   discard w.setCursorPosCallback(cursorPosCallback)
   discard w.setFramebufferSizeCallback(frameSizeCallback)
+
   var width, height: int32
   w.getFramebufferSize(width.addr, height.addr)
   w.frameSizeCallback(width, height)
 
   var game = Game()
   game.init()
+
+  when not defined(release):
+    paravim.init(game, w)
 
   game.totalTime = glfwGetTime()
 
@@ -54,6 +84,9 @@ when isMainModule:
     game.deltaTime = ts - game.totalTime
     game.totalTime = ts
     game.tick()
+    when not defined(release):
+      if not focusOnGame:
+        paravim.tick(game)
     w.swapBuffers()
     glfwPollEvents()
 
